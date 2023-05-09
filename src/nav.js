05561eed9b4db499678764a64f73961d5ead2470
Kyle <khughessean01@gmail.com>
2023-05-09 15:13:43 +0200

    Nav refactor and cleanup

--- src/nav.js
+++ src/nav.js
@@ -43,34 +43,19 @@ function createLogoLink() {
 function createAuthentication() {
 	const auth = document.createElement('div');
 	auth.classList.add('auth');
-	const signUpButton = createSignUpButton();
-	const signInButton = createSignInButton();
+	const signUpButton = createButton('SIGN UP');
+	const signInButton = createButton('SIGN IN');
 	auth.appendChild(signUpButton);
 	auth.appendChild(signInButton);
 	return auth;
 }
 
-function createSignUpButton() {
-	const signUpButton = document.createElement('button');
-	signUpButton.textContent = 'Sign up';
-	return signUpButton;
-}
-
-function createSignInButton() {
-	const signInButton = document.createElement('button');
-	signInButton.textContent = 'Sign in';
-	return signInButton;
-}
-
 function createMiddleButtons() {
 	const middleButtons = document.createElement('div');
 	middleButtons.classList.add('middle-buttons');
-	const button1 = createButton('Button 1');
-	const button2 = createButton('Button 2');
-	const button3 = createButton('Button 3');
-	button1.textContent = 'Home';
-	button2.textContent = 'Menu';
-	button3.textContent = 'Contact';
+	const button1 = createButton('Home');
+	const button2 = createButton('Menu');
+	const button3 = createButton('Contact');
 	middleButtons.appendChild(button1);
 	middleButtons.appendChild(button2);
 	middleButtons.appendChild(button3);
@@ -82,42 +67,3 @@ function createButton(text) {
 	button.textContent = text;
 	return button;
 }
-
-// _________________________-
-// export function createNav() {
-// 	const navContainer = createNavContainer();
-// 	const nav = createNavigation();
-// 	const logo = createLogo();
-// 	const auth = createAuthentication();
-// 	const middleButtons = createMiddleButtons();
-
-// 	nav.appendChild(logo);
-// 	nav.appendChild(middleButtons);
-// 	nav.appendChild(auth);
-// 	navContainer.appendChild(nav);
-
-// 	return navContainer;
-// }
-
-// function createMiddleButtons() {
-// 	const middleButtons = document.createElement('div');
-// 	middleButtons.classList.add('middle-buttons');
-// 	const button1 = createButton('Button 1');
-// 	const button2 = createButton('Button 2');
-// 	const button3 = createButton('Button 3');
-// 	button1.textContent = 'Home';
-// 	button2.textContent = 'Menu';
-// 	button3.textContent = 'Contact';
-// 	middleButtons.appendChild(button1);
-// 	middleButtons.appendChild(button2);
-// 	middleButtons.appendChild(button3);
-// 	return middleButtons;
-// }
-
-// function createButton(text) {
-// 	const button = document.createElement('button');
-// 	button.textContent = text;
-// 	return button;
-// }
-
-// rest of the functions stay the same
